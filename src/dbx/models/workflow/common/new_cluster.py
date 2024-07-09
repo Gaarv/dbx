f3e562cdf55fe89f1e27c4e5492078cee048d493
Gaarv <Gaarv@users.noreply.github.com>
2024-07-09 15:07:56 +0200

    change from root validator to model validator

--- src/dbx/models/workflow/common/new_cluster.py
+++ src/dbx/models/workflow/common/new_cluster.py
@@ -9,7 +9,7 @@ class AutoScale(FlexibleModel):
     min_workers: int
     max_workers: int
 
-    @root_validator()
+    @model_validator(mode="before")
     def _validate(cls, values):  # noqa
         assert values["max_workers"] > values["min_workers"], ValueError(
             f"""
